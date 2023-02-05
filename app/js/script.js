e9376e3bebd85acaa3ba7b5d84aa6b0802d85e26
Vince Mendella <vmendella@gmail.com>
2023-02-04 20:54:27 -0600

    working on the responsive aspects of the header menu

--- app/js/script.js
+++ app/js/script.js
@@ -7,43 +7,29 @@ const overlay = document.querySelector('.overlay');
 const mobileMenu = document.querySelector('.mobileMenu');
 const nav__list = document.querySelector('.nav__list');
 const nav__item = document.querySelector('.nav__item');
-// const mobile__fade = document.querySelector('.mobile__fade');
 
 btnHamburger.addEventListener('click', function() {
-  console.log('click hamburger menu');
+  console.log('click hamburger');
 
-  if( header.classList.contains('open') ) { // Close Hamburger Menu
+  if(header.classList.contains('open')) { // Close hamburger menu
     body.classList.remove('noscroll');
     header.classList.remove('open');
     overlay.classList.remove('fade-in');
     overlay.classList.add('fade-out');
-    mobileMenu.classList.remove('fade-in');
-    mobileMenu.classList.add('hide-for-mobile');
-    // mobile__fade.classList.remove('fade-in');
-    // mobile__fade.classList.add('fade-out-menu');
-    // nav__list.classList.add('nav__list');
     nav__list.classList.remove('fade-in');
     mobileMenu.classList.remove('nav-open');
     mobileMenu.classList.add('hide-for-mobile');
-    nav__list.classList.remove('nav__list--open');
-  }
-  else { // Open Hamburger Menu
+    nav__item.classList.remove('nav__item--open');
+  } else { // Open hamburger Menu
+    console.log('else menu');
+
     body.classList.add('noscroll');
     header.classList.add('open');
-    overlay.classList.remove('fade-out');
     overlay.classList.add('fade-in');
-    // mobile__fade.classList.add('fade-in');
-    // mobile__fade.classList.remove('fade-our-menu');
+    overlay.classList.remove('fade-out');
+    nav__list.classList.add('fade-in');
     mobileMenu.classList.add('nav-open');
     mobileMenu.classList.remove('hide-for-mobile');
-    nav__list.classList.add('nav__list--open');
-    nav__list.classList.remove('nav__list');
+    nav__item.classList.add('nav__item--open');
   }
-
 });
-
-const message = () => {
-  console.log('The javascript was transpiled');
-}
-
-message();
