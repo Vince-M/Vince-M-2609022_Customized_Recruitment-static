aec695433d1c4cefe9eb2cbab0fa3a1c5336fd58
Vince Mendella <vmendella@gmail.com>
2023-02-14 20:37:20 -0600

    add fadeIn animation for testimonial boxes index and about pages

--- app/js/script.js
+++ app/js/script.js
@@ -8,6 +8,8 @@ const mobileMenu = document.querySelector('.mobileMenu');
 const nav__list = document.querySelector('.nav__list');
 const nav__item = document.querySelector('.nav__item');
 
+const faders = document.querySelectorAll('.fadeIn');
+
 btnHamburger.addEventListener('click', function() {
   console.log('click hamburger');
 
@@ -33,3 +35,27 @@ btnHamburger.addEventListener('click', function() {
     nav__item.classList.add('nav__item--open');
   }
 });
+
+const appearOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px -100px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScoll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+  })
+},
+appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
